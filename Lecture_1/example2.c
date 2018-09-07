eb2abfee715cc7f9703c48c3b1bb3502789e2ad4
Harsh Gupta <harshgupta2204@live.com>
2018-09-07 16:23:15 -0400

    Added some linux bash commands

--- Lecture_1/example2.c
+++ Lecture_1/example2.c
@@ -8,4 +8,6 @@ int main()
 
 	printf("Enter x:");
 	scanf("%d",&x); //& directs to the address of the variable d. Therefore, &d is a memory address
+	z = x+2.4;
+	printf("z is %4.2f\n",z);
 }
---
+++ Lecture_2/notes.md
@@ -0,0 +1,12 @@
+##LECTURE 2
+
+Learn LINUX BASH!
+
+
+ls
+rm
+mv
+cd
+man
+chmod
+
